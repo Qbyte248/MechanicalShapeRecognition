f3af70531a6f124926c46cc555c02813684a0e3e
Qbyte248 <formelwiki@web.de>
2016-11-15 12:52:54 +0100

    Test tacho count

--- src/Test/TestMain.java
+++ src/Test/TestMain.java
@@ -1,10 +1,21 @@
 package Test;
 
+import lejos.nxt.*;
+
 public class TestMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		System.out.println("Hello World");
+
+		while (true) {
+			// get tacho count
+			int tachoCount = Motor.A.getTachoCount();
+
+			// show tacho count on screen
+			LCD.drawString("Tacho count: " + tachoCount, 1, 1);
+
+		}
 	}
 
 }

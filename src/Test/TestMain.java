9d86410dab7892be73234f205deb242eaa1a7491
Garcko <timsteindel@gmail.com>
2016-11-17 15:42:58 +0100

    Update TestMain.java

--- src/Test/TestMain.java
+++ src/Test/TestMain.java
@@ -7,7 +7,7 @@ public class TestMain {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World");
-
+//testing
 		while (true) {
 			// get tacho count
 			int tachoCount = Motor.A.getTachoCount();

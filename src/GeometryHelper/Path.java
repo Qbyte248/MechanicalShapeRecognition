b4d36aef0e11045f562df50cdc32c3dfedc3aa0d
Qbyte248 <formelwiki@web.de>
2016-11-29 08:15:54 +0100

    Implemented scaleBy translateBy in Path and Shape. Moved smallest box function in ShapePotentialClassifier to Shape.getRectangle method

--- src/GeometryHelper/Path.java
+++ src/GeometryHelper/Path.java
@@ -10,4 +10,31 @@ public class Path {
 	public void addPoint(double x, double y) {
 		points.add(new Vector(x, y));
 	}
+	
+	/**
+	 * @param scaleFactor
+	 * @param center from which it is scaled (this/Path coordinate system)
+	 */
+	public void scaleBy(double scaleFactor, Vector center) {
+		Vector newPoint;
+		for(int i = 0; i < points.size(); i++) {
+			newPoint = points.get(i);
+			newPoint.addInPlace(origin);
+			newPoint.subtractInPlace(center);
+			newPoint.multiplyInPlace(scaleFactor);
+			newPoint.addInPlace(center);
+		}
+		
+		newPoint = origin;
+		newPoint.subtractInPlace(center);
+		newPoint.multiplyInPlace(scaleFactor);
+		newPoint.addInPlace(center);
+	}
+	
+	/**
+	 * @param dv translation Vector
+	 */
+	public void translateBy(Vector dv) {
+		origin.addInPlace(dv);
+	}
 }
--- src/GeometryHelper/Shape.java
+++ src/GeometryHelper/Shape.java
@@ -3,14 +3,71 @@ package GeometryHelper;
 import java.util.ArrayList;
 
 public class Shape {
-	public ArrayList<Path > paths= new ArrayList ();
+	public ArrayList<Path> paths= new ArrayList<>();
 	public Vector origin;
 	
-	public void scaleBy(double scaleFactor) {
-		// TODO
+	// possible optimization to set all origins to (0, 0) and add a (boolean) flag 
+	
+	private Rectangle rectangle;
+	
+	/**
+	 * @return Smallest rectangle which contains `this`
+	 * @note This method only recalculates rectangle if necessary (this.rectangle == null)
+	 */
+	public Rectangle getRectangle() {
+		if (this.rectangle != null) {
+			return this.rectangle;
+		} else {
+			return this.getRectangleRecalculate();
+		}
+	}
+	
+	/**
+	 * @return Smallest rectangle which contains `this`
+	 * @note This method always recalculates the rectangle O(n)
+	 */
+	public Rectangle getRectangleRecalculate() {
+		Vector lowerLeft = new Vector(Double.MAX_VALUE, Double.MAX_VALUE);
+		Vector upperRight = new Vector(-Double.MAX_VALUE, -Double.MAX_VALUE);
+		
+		boolean hasPoints = false;
+		for (Path path : this.paths) {
+			hasPoints = hasPoints || !path.points.isEmpty();
+			for (Vector point : path.points) {
+				Vector v = this.origin.add(path.origin.add(point));
+				
+				// assign lower left and upper right
+				lowerLeft.x = Math.min(lowerLeft.x, v.x);
+				lowerLeft.y = Math.min(lowerLeft.y, v.y);
+				upperRight.x = Math.max(upperRight.x, v.x);
+				upperRight.y = Math.max(upperRight.y, v.y);
+			}
+		}
+		
+		if (!hasPoints) {
+			throw new IllegalArgumentException("Shape has no points => no smallest box");
+		}
+		
+		this.rectangle = new Rectangle(lowerLeft, upperRight.subtract(lowerLeft));
+		return this.rectangle;
+	}
+	
+	/**
+	 * @param scaleFactor
+	 * @param center from which it is scaled (this/Shape coordinate system)
+	 */
+	public void scaleBy(double scaleFactor, Vector center) {
+		Vector pathCenter = center.subtract(origin);
+		for (Path path : paths) {
+			path.scaleBy(scaleFactor, pathCenter);
+		}
+		
+		origin.subtractInPlace(center);
+		origin.multiplyInPlace(scaleFactor);
+		origin.addInPlace(center);
 	}
 	
 	public void translateBy(Vector dv) {
-		// TODO
+		origin.addInPlace(dv);
 	}
 }

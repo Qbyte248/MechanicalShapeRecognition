925e63dfcf863de3d24edc597e1217c804fa8252
Qbyte248 <formelwiki@web.de>
2016-11-29 11:54:43 +0100

    Added test class

---
+++ src/QualityControl/ClassificationTest.java
@@ -0,0 +1,15 @@
+package QualityControl;
+
+import Classification.*;
+
+public class ClassificationTest {
+
+	public static void main(String[] args) {
+		
+		ShapeClassifier classifier = new ShapePotentialClassifier();
+		
+		String description = classifier.classify(ShapeTemplates.get("A"));
+		System.out.println("description" + description);
+	}
+
+}
